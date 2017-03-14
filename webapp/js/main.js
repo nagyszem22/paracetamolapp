c595f3a2a1b46974be3f049ea324b8c694780d49
nagyszem22 <takacsmarton22@gmail.com>
2017-03-14 01:00:03 +0100

    frontend

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	requesData = {};
+	/*requesData = {};
 	requesData.currentDateTime = "11-05-17 18:45:00";
 	requesData.name = "John Doe";
 	requesData.birthday = "11-05-17";
@@ -15,6 +15,18 @@ $(document).ready(function(){
 		success: function(data) {
 	        console.log(data);
 	        // $('.hello').html(data.name);
-	 }});
+	 }}); */
+
+
+	/* Toggle help text */
+	$('.help-button').on('click', function() {
+		$(this).parent('.input-label').parent('.input-box').children('.help-text').toggle();
+	});
+
+	/* Show next page */
+	$('.next-button').on('click', function() {
+		var link = $(this).attr('data-link');
+		window.location.href = link;
+	});
 
 });
